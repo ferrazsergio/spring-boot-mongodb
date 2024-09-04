8576ff9bab744c5c9b888b2b74d24729a313ae6c
ferrazsergio <juniorsilva6677@hotmail.com>
2024-09-03 22:22:19 -0300

    Consulta com varios criterios

--- src/main/java/com/ferrazsergio/springmongo/repository/PostRepository.java
+++ src/main/java/com/ferrazsergio/springmongo/repository/PostRepository.java
@@ -1,13 +1,22 @@
 package com.ferrazsergio.springmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ferrazsergio.springmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String>{
+		@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+		List<Post> searchTitle(String text);
+	
+	
 		List<Post> findByTitleContaingIgnoreCase(String text);
+		
+		@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+		List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/ferrazsergio/springmongo/resources/PostResource.java
+++ src/main/java/com/ferrazsergio/springmongo/resources/PostResource.java
@@ -1,11 +1,13 @@
 package com.ferrazsergio.springmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,7 +35,6 @@ public class PostResource {
 			return ResponseEntity.status(500).body(null);
 		}
 	}
-	
 
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
@@ -44,6 +45,18 @@ public class PostResource {
 		} catch (Exception e) {
 			return ResponseEntity.status(500).body(null);
 		}
-	
-}
+	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+
+	}
+
 }
